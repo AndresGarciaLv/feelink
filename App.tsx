15252c5c520a8961e5799ab1b462804bd15509a8
Andrea <132911113+Goraz23@users.noreply.github.com>
2025-05-20 20:34:38 -0500

    Se hace enmaquetado de login y register

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
 import BluetoothScreen from './src/BluetoothScreen';
+import AuthForm from './src/views/loginView';
 
 export default function App() {
   return (
+    <>
     <SafeAreaView style={{ flex: 1 }}>
-      <BluetoothScreen />
+      <AuthForm/>  
     </SafeAreaView>
+    
+    </>
   );
 }
